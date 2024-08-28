4ae9f52ecaafc3fdd3f821cb3d2051fbb5231b31
Charlie Kisylia <kisyliac@yahoo.com>
2024-08-28 08:31:48 -0700

    RockPaperScissors on the Frontend

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -2,14 +2,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import './css/Welcome.css';
+import RockPaperScissors from './RockPaperScissors';
 
 const Welcome = () => {
     const { username } = useParams();
 
     return (
         <div className="welcome-container">
-            <h1>Hello {username}</h1>
+            <div className="username-container">
+                <h1>Hello {username}</h1>
+            </div>
+            <div className="game-container">
+                <RockPaperScissors username={username} />
+            </div>
         </div>
+        
     );
 };
 
