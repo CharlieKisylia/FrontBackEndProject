d38b631456db74b1d1786c54dce5b4173ca28f2c
Charlie Kisylia <kisyliac@yahoo.com>
2024-09-19 08:05:19 -0700

    Make username display properly in upper right

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom'; // Import useParams to get the URL parameters
 import NavBar from './NavBar'; // Import the NavBar component
 import RockPaperScissors from './RockPaperScissors'; // Import the RockPaperScissors component
 import TicTacToe from './TicTacToe'; // Import the TicTacToe component
 import './css/Welcome.css'; // Import the CSS
 
 const Welcome = () => {
+  const { username } = useParams(); // Get the username from the URL
   const [selectedGame, setSelectedGame] = useState('RockPaperScissors'); // Default to RockPaperScissors
-  const username = 'charliek'; // Set username here or retrieve it from a higher-level component/context
 
   const handleGameChange = (game) => {
     setSelectedGame(game);
@@ -16,7 +17,7 @@ const Welcome = () => {
     <>
       <div className="welcome-page">
         <div className="username-container">
-            Hello {username}
+            Hello {username} {/* Now it will show the dynamic username */}
         </div>
         <NavBar onGameChange={handleGameChange} />
         <div className="game-content">
